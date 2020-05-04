36878afd1c515836367d148b8cd4ae14eed675d3
tsygankov <tsygankov@magora-systems.com>
2020-05-04 14:44:07 +0700

    Updates

---
+++ precache-manifest.b890feb2c0beb0ba3cea209e61c7dd88.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d0a618f2b3e3f34145ed348a305561",
+    "url": "/promo/index.html"
+  },
+  {
+    "revision": "65710212e6e507140456",
+    "url": "/promo/static/css/main.61f47e84.chunk.css"
+  },
+  {
+    "revision": "b314762695dabbd32917",
+    "url": "/promo/static/js/2.4702accc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/promo/static/js/2.4702accc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65710212e6e507140456",
+    "url": "/promo/static/js/main.ec926a1a.chunk.js"
+  },
+  {
+    "revision": "ea3437ff50ae9cbf1c8f",
+    "url": "/promo/static/js/runtime-main.52211e28.js"
+  },
+  {
+    "revision": "4307d7bad82729169e304b76363c6df6",
+    "url": "/promo/static/media/Geometria.4307d7ba.ttf"
+  },
+  {
+    "revision": "bb2169a5e421517339851c1ff6f676ae",
+    "url": "/promo/static/media/Rectangle-45.bb2169a5.png"
+  },
+  {
+    "revision": "93b09f5cdcdf1eef0644d25c335e8e5d",
+    "url": "/promo/static/media/Rectangle-46.93b09f5c.png"
+  },
+  {
+    "revision": "3d6bba5e17dae83f7fc119016029bbbd",
+    "url": "/promo/static/media/Rectangle-47.3d6bba5e.png"
+  },
+  {
+    "revision": "568c6179195e4e70b1f85331ea67c38b",
+    "url": "/promo/static/media/Rectangle-48.568c6179.png"
+  },
+  {
+    "revision": "90c85d26b85321f926433023dddd712d",
+    "url": "/promo/static/media/Rectangle-49.90c85d26.png"
+  },
+  {
+    "revision": "03580f506748699f2da544b602243f5e",
+    "url": "/promo/static/media/Rectangle-50.03580f50.png"
+  }
+]);
